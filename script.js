02e6a5a68eb3729c4910ba632c643e5e3b51c2bc
Austin Adam <austin.s.adam@gmail.com>
2022-01-27 09:48:08 -0800

    final commit, custom backgrounds added

---
+++ script.js
@@ -0,0 +1,25 @@
+const body = document.body;
+// const {body} = document;
+
+function changeBackground(number){
+    //Check if background already selected
+    let prevBackground; 
+    if (body.className) {
+        prevBackground = body.className;
+    }
+    //Reset CSS class for body
+    body.className = '';
+    switch (number) {
+        case '1':
+            return (prevBackground === 'background-1' ? false : body.classList.add('background-1'));
+            break;
+        case '2':
+            return (prevBackground === 'background-2' ? false : body.classList.add('background-2'));
+            break;
+        case '3':
+            return (prevBackground === 'background-3' ? false : body.classList.add('background-3'));
+            break;    
+        default:
+            break;
+    }
+}
